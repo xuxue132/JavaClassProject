b45f4b84d03582d678031f7bf17126b788b9bce4
xuxue132 <1324748556@qq.com>
2019-07-22 22:22:10 +0800

    fix one file

--- src/day1/Exerclses/Exerclses5_17_1.java
+++ src/day1/Exerclses/Exerclses5_17_1.java
@@ -8,17 +8,17 @@ public class Exerclses5_17_1 {
         Scanner input=new Scanner(System.in);
         int line=input.nextInt();
 
-        for(int i=1;i<=line;i++){
-            for(int j=1;j<=2*line-2*i;j++){
-                System.out.print(" ");
-            }
-            for(int k=i;k>=1;k--){
-                System.out.print(k+" ");
-            }
-            for(int h=2;h<=i;h++){
-                System.out.print(h+" ");
-            }
-            System.out.println();
-        }
+//        for(int i=1;i<=line;i++){
+//            for(int j=1;j<=2*line-2*i;j++){
+//                System.out.print(" ");
+//            }
+//            for(int k=i;k>=1;k--){
+//                System.out.print(k+" ");
+//            }
+//            for(int h=2;h<=i;h++){
+//                System.out.print(h+" ");
+//            }
+//            System.out.println();
+//        }
     }
 }
